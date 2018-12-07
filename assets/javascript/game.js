efe2e0b13cda559dd45af5e7706dcd9cec142d16
Brandon Dixon <bdixon850@yahoo.com>
2018-12-07 00:05:44 -0500

    last commit

---
+++ assets/javascript/game.js
@@ -0,0 +1,88 @@
+var computerChoices = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+
+
+let wins = 0;
+let losses = 0;
+let guesses = 9;
+let guessesLeft = 9;
+let guessedLetters = [];
+var letterToGuess = null;
+var yay = new Audio
+var boo = new Audio
+var ouch = new Audio
+
+
+var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+document.querySelector('.jadesgame').style.display = "none";
+
+function updateGuessesLeft() {
+    
+    document.querySelector('#guessLeft').innerHTML = "Guesses left: " + guessesLeft;
+};
+
+function updateLetterToGuess() {
+    this.letterToGuess = this.computerChoices[Math.floor(Math.random() * this.computerChoices.length)];
+};
+
+function updateGuessesSoFar() {
+    
+    document.querySelector('#let').innerHTML = "Your Guesses so far: " + guessedLetters.join(', ');
+};
+
+var reset = function() {
+    totalGuesses = 9;
+    guessesLeft = 9;
+    guessedLetters = [];
+    setTimeout(function() { document.querySelector('.jadesgame').style.display = 'none'; }, 5000);
+    updateLetterToGuess();
+    updateGuessesLeft();
+    updateGuessesSoFar();
+}
+
+updateLetterToGuess();
+updateGuessesLeft();
+
+
+document.onkeyup = function(event) {
+    var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
+    var check = computerChoices.includes(userGuess);
+
+    if (check === false) {
+        ouch.play();
+        alert("That was not a valid guess, try again?");
+        return false;
+    } else if (check === true) {
+        
+        guessesLeft--;
+        guessedLetters.push(userGuess);
+        updateGuessesLeft();
+        updateGuessesSoFar();
+
+        if (guessesLeft > 0) {
+            if (userGuess == letterToGuess) {
+                wins++;
+                yay.play();
+                document.querySelector('#wins').innerHTML = "Wins: " + wins;
+                userGuess = userGuess.toUpperCase();
+                document.querySelector('.jadesgame').style.display = '';
+                document.querySelector('.jadesgame').style.height = '4em';
+                document.querySelector('.jadesgame').innerHTML = "Good guess! " + userGuess + " was the letter I had in mind!";
+                reset();
+            }
+        } else if (guessesLeft == 0) {
+            
+            losses++;
+            boo.play();
+            document.querySelector('#losses').innerHTML = "Losses: " + losses;
+            document.querySelector('.jadesgame').style.display = '';
+            document.querySelector('.jadesgame').innerHTML = "Sorry, I was thinking of the letter " + letterToGuess;
+            
+            reset();
+
+        }
+        return false;
+    } else {
+        alert("Oops, we have an error");
+    }
+
+};
